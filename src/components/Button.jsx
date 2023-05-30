3f1b2ecad6e136d4bede705a653d494c24192927
Francesca Santoriello <f.santoriello@hotmail.com>
2023-05-30 11:09:58 +0200

    Business + FeatureCard

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,10 +1,13 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react'
 
-const Button = () => {
+// eslint-disable-next-line react/prop-types
+const Button = ({ styles }) => {
   return (
     <div>
-      
+      <button type='button' className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none ${styles} rounded-[10px]`}>
+        Get Started
+      </button>
     </div>
   )
 }
