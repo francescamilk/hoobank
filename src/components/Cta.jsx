fa3d5c4e2e0e577e9cff2a464d291bd38e019f4c
Francesca Santoriello <f.santoriello@hotmail.com>
2023-05-30 12:29:11 +0200

    CTA

--- src/components/Cta.jsx
+++ src/components/Cta.jsx
@@ -1,12 +1,17 @@
-// eslint-disable-next-line no-unused-vars
-import React from 'react'
+import styles from '../style'
+import Button from './Button'
 
-const Cta = () => {
-  return (
-    <div>
-      
+const Cta = () => (
+  <section className={`${styles.flexCenter} ${styles.marginY} ${styles.padding} sm:flex-row flex-col bg-black-gradient-2 rounded-[20px] box-shadow`}>
+    <div className='flex-1 flex flex-col'>
+      <h2 className={styles.heading2}>
+        Let&apos;s try our service now!
+      </h2>
+      <p className={`${styles.paragraph} max-w-[470px] mt-5`}>Everything you need to accept card payments and grow your business anywhere in the planet.</p>
     </div>
-  )
-}
+    
+    <Button className={`${styles.flexCenter} sm:ml-10 ml-0 mt-10`} />
+  </section>
+)
 
 export default Cta
